0ee37908e4b1a279a13f4ba22ec6d8d9c8c010f2
Jose Jimenez <email@jimenezjose.com>
2021-04-23 18:17:43 -0500

    feat: Make the function validate the input value

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function myFunction(arr = []) {
+function myFunction(arr) {
+  if (
+    !Array.isArray(arr) ||
+    arr.length < 2 ||
+    !arr.every((el) => typeof el === "number" && parseInt(el) > 0)
+  ) {
+    throw new Error(
+      "You must provide an array with at least two positives integers"
+    );
+  }
+
   return arr;
 }
 
