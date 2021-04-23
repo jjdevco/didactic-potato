0ed5cd8b9156ae6cde6a5d0c7d2122eaccbd0282
Jose Jimenez <email@jimenezjose.com>
2021-04-23 18:17:25 -0500

    test: It should check the input value

--- index.test.js
+++ index.test.js
@@ -2,8 +2,23 @@ const myFunction = require("./index");
 
 describe("myFunction", () => {
   test("It should return an array", () => {
-    const result = myFunction();
-
+    const result = myFunction([1, 2]);
     expect(result).toBeInstanceOf(Array);
+  });
+
+  test("The function should throw an error if at least two positives integers were not provided", () => {
+    const errorText =
+      "You must provide an array with at least two positives integers";
+    expect(() => {
+      myFunction([]);
+    }).toThrow(errorText);
+
+    expect(() => {
+      myFunction(["a", undefined, {}]);
+    }).toThrow(errorText);
+
+    expect(() => {
+      myFunction([5, 20, -7, 13, 0]);
+    }).toThrow(errorText);
   });
 });
